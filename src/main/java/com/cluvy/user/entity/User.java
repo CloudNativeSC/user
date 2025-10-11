7ec27b6f563b1c13aca26ccbf341fbce2f6513e7
melonhong <melonhong46@gmail.com>
2025-10-11 22:41:22 +0900

    fix: user 테이블 이름을 users로 변경
    - PostgreSQL에 user 테이블이 있기 때문에 부득이하게 이름을 변경함.

--- src/main/java/com/cluvy/user/entity/User.java
+++ src/main/java/com/cluvy/user/entity/User.java
@@ -13,6 +13,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "users")
 public class User extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
