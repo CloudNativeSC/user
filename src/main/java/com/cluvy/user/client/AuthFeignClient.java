a8504adb37cf453ee10b66eeee1960af28181187
melonhong <melonhong46@gmail.com>
2025-10-13 13:54:52 +0900

    feat: 외부 API URL을 환경변수를 사용하도록 수정

--- src/main/java/com/cluvy/user/client/AuthFeignClient.java
+++ src/main/java/com/cluvy/user/client/AuthFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "auth-service", url = "http://localhost:8080")
+@FeignClient(name = "auth-service", url = "http://${auth.host}:${auth.port}")
 public interface AuthFeignClient {
     @GetMapping("/api/auth/me")
     Long getUserIdFromToken(@RequestHeader("Authorization") String authorizationHeader);
